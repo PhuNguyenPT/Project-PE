91773bb4eae7d7f0559f33b5b6792bcb60bb489a
Tvo <149646715+TonyVoo@users.noreply.github.com>
2024-07-01 09:22:22 +0700

    Update LoginPage.vue

--- Shopping_Cart/src/components/LoginPage.vue
+++ Shopping_Cart/src/components/LoginPage.vue
@@ -1,105 +1,97 @@
 <template>
     <div class="container">
-    <body>
-        <form id="signin-form" @submit.prevent="handleSubmit">
+      <form id="signin-form" @submit.prevent="handleSubmit">
         <h1>Sign in</h1>
         <div class="input-field">
-            <label for="email-username">Email or Username:</label>
-            <input type="text" required v-model="Email">
-            <div v-if="loginError">{{ loginError }}</div>
+          <label for="email-username">Email or Username:</label>
+          <input type="text" required v-model="Email">
+          <div v-if="loginError" class="error-message">{{ loginError }}</div>
         </div>
-
+  
         <div class="input-field">
-            <label for="password">Password:</label>
-            <input type="password" required v-model="Password">
-            <div v-if="passError">{{ passError }}</div>
+          <label for="password">Password:</label>
+          <input type="password" required v-model="Password">
+          <div v-if="passError" class="error-message">{{ passError }}</div>
         </div>
-
+  
         <button type="submit" class="btn-continue">Continue</button>
-
+  
         <div class="signup-link">
-            <p>No account? <router-link to='/SignupPage'>Sign up</router-link> for browsing product</p>
+          <p>No account? <router-link to='/SignupPage'>Sign up</router-link> for browsing product</p>
         </div>
-    </form>
-    </body>
+        <div v-if="successMessage" class="success-message">{{ successMessage }}</div>
+      </form>
     </div>
-    
-</template>
-
-<script>
-import store from '../store'
-
-export default {
-    data(){
-            return {
-                Email: '',
-                Password:'',
-                loginError:'',
-                passError:''
-            }
-        },
-        methods: {
-            handleSubmit(){                
-                this.passError = this.Password.length > 7 ? '' : 'Password must be 8 chars or more'
-
-                if(!this.passError){
-                    let json = {
-                        "email": this.Email,
-                        "password": this.Password,
-                    }
-
-                    const options = {
-                        method: 'POST',
-                        headers: {'Content-Type': 'application/json'},
-                        body: JSON.stringify(json),
-                    };
-
-                
-                    fetch('https://localhost:443/api/v1/auth/login', options)
-                    .then(response => response.json())
-                    .then(response => {
-                        console.log(response)
-                        if(response.error){
-                            this.loginError = response.error;
-                        }
-                        else{
-                            store.mutations.setToken(response.token)
-                            store.mutations.setEmail(this.Email)
-                            this.$router.push('./');
-                        }
-                        
-                    })
-                    .catch(err => console.error(err));
-                }
-
-                
-            }
+  </template>
+  
+  <script>
+  import store from '../store'
+  
+  export default {
+    data() {
+      return {
+        Email: '',
+        Password: '',
+        loginError: '',
+        passError: '',
+        successMessage: ''
+      }
+    },
+    methods: {
+      handleSubmit() {
+        this.passError = this.Password.length > 7 ? '' : 'Password must be 8 chars or more'
+  
+        if (!this.passError) {
+          let json = {
+            "email": this.Email,
+            "password": this.Password,
+          }
+  
+          const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(json),
+          };
+  
+          fetch('https://localhost:443/api/v1/auth/login', options)
+            .then(response => response.json())
+            .then(response => {
+              console.log(response)
+              if (response.error) {
+                this.loginError = response.error;
+                this.successMessage = '';
+              } else {
+                store.mutations.setToken(response.token)
+                store.mutations.setEmail(this.Email)
+                this.successMessage = 'Login successful! Redirecting...';
+                this.loginError = '';
+                setTimeout(() => {
+                  this.$router.push('./');
+                }, 2000);
+              }
+            })
+            .catch(err => {
+              console.error(err);
+              this.loginError = 'Failed to login. Please try again.';
+              this.successMessage = '';
+            });
         }
-}
-</script>
-
-
-<style>
-.container {
-    /* display: flex; */
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  .container {
     justify-content: center;
     align-items: center;
-}
-
-body {
-    font-family: Arial, sans-serif;
-    background-color: #fff;
+  }
+  
+  header h1 {
     margin: 0;
-    padding: 0;
-
-}
-
-
-header h1 {
-    margin: 0;
-}
-
-form {
+  }
+  
+  form {
     background-color: #fff;
     padding: 30px;
     border-radius: 5px;
@@ -110,31 +102,31 @@ form {
     margin-top: 80px;
     margin-left: auto;
     margin-right: auto;
-}
-
-h1 {
+  }
+  
+  h1 {
     text-align: center;
     margin-bottom: 20px;
-}
-
-.input-field {
-    margin-bottom: 20px;    
-}
-
-.input-field label {
+  }
+  
+  .input-field {
+    margin-bottom: 20px;
+  }
+  
+  .input-field label {
     display: block;
     font-weight: bold;
     margin-bottom: 5px;
-}
-
-.input-field input {
+  }
+  
+  .input-field input {
     width: 100%;
     padding: 10px;
     border: 1px solid #ccc;
     border-radius: 3px;
-}
-
-.btn-continue {
+  }
+  
+  .btn-continue {
     background-color: #000000;
     color: #fff;
     border: none;
@@ -143,15 +135,28 @@ h1 {
     cursor: pointer;
     width: 100%;
     font-size: 16px;
-}
-
-.signup-link {
+  }
+  
+  .signup-link {
     margin-top: 20px;
     text-align: center;
-}
-
-.signup-link a {
+  }
+  
+  .signup-link a {
     color: #007185;
     text-decoration: none;
-}
-</style>
+  }
+  
+  .error-message {
+    color: red;
+    font-size: 14px;
+  }
+  
+  .success-message {
+    color: green;
+    font-size: 14px;
+    text-align: center;
+    margin-top: 20px;
+  }
+  </style>
+  
